5b1d68b65b1e479d30a800665d3b1566d0ba133f
rxt3820 <rxt3820@rit.edu>
2019-11-19 21:22:32 -0500

    update index with the php

--- basixModular/index.php
+++ basixModular/index.php
@@ -3,6 +3,38 @@
     include('header.html');
 ?>
 
+<?php
+        $page = 'home';
+		 require 'dbConnect.inc';  
+		$sql = "SELECT content FROM home where page='$page'";
+		$result = $mysqli->query($sql);
+
+		if($result->num_rows > 0){
+			//output the data for each row
+			while ($row = $result->FETCH_ASSOC()) {
+				echo $row['content'];
+			}
+		}else{
+			echo "0 results";
+		}
+	?>
+
+
+	<?php
+        $page = 'home';
+		 require 'dbConnect.inc';  
+		$sql = "SELECT internalCSS FROM home where page='$page'";
+		$result = $mysqli->query($sql);
+
+		if($result->num_rows > 0){
+			//output the data for each row
+			while ($row = $result->FETCH_ASSOC()) {
+				echo $row['internalCSS'];
+			}
+		}else{
+			echo "0 results";
+		}
+	?>
 <!DOCTYPE html>
 
 <html>
