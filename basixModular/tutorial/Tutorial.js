bb8f392b17b7e19bcbcddf9c3ca84045cc15e01d
Julianna Baker <17jbaker.stem@gmail.com>
2019-11-29 14:53:32 -0700

    Changes to the tutorial page

--- basixModular/tutorial/Tutorial.js
+++ basixModular/tutorial/Tutorial.js
@@ -1,21 +1,27 @@
-var result = document.getElementById('result');
-var submit = document.getElementById('submit');
+console.log("tutorial.js has loaded");
 
-function getResult () {
-  var answer1 = document.getElementById('q1').value;
-  var answer2 = document.getElementById('q2').value;
-  
-if (answer1 == '0') {
-  if (answer2 == '222') {
-      result.innerHTML = '<p class="correct">You got the answer!</p>';
-    } else {
-      result.innerHTML = '<p class="wrong">Sorry try again!</p>';
-    }
+let result = document.getElementById("result");
+let submit = document.getElementById("submit");
+
+function getResult() {
+  let answer1 = document.getElementById("q1").value;
+  let answer2 = document.getElementById("q2").value;
+
+  console.log("answer1", answer1, "answer2", answer2);
+
+  if (answer1 == "0") {
+    result.innerHTML = '<p class="correct">You got the answer!</p>';
+  } else {
+    result.innerHTML = '<p class="wrong">Sorry try again!</p>';
+  }
+
+  if (answer2 == "222") {
+    result.innerHTML = '<p class="correct">You got the answer!</p>';
   } else {
     result.innerHTML = '<p class="wrong">Sorry try again!</p>';
   }
 }
 
-submit.addEventListener('click', getResult());
-
-
+if (submit && submit.length) {
+  submit.addEventListener("click", getResult());
+}
