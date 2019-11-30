dd43683aba48ff72508ece76b8c1d35c690238ed
rxt3820 <rxt3820@rit.edu>
2019-11-29 22:10:18 -0500

    changes

--- basixModular/ProcessLogin.php
+++ basixModular/ProcessLogin.php
@@ -5,25 +5,26 @@ require "../dbConnect.inc";
 
 session_start();
 
-$user = trim($_POST['uname']);
+$user = trim($_POST['username']);
 $_SESSION['loggedIn'] = false;
 
 if ($mysqli) {
     if (isset($user) && isset($_POST['psw'])) {
-    $pass = $_POST['psw'];
+    $pass = $_POST['password'];
 
-    $query = 'SELECT * FROM LoginForm where user = "'.$_POST['uname'].'" AND password = "'.$_POST['psw'].'"';
+    $query = 'SELECT * FROM LoginForm where user = "'.$user.'" AND password = "'.$pass.'"';
+    echo $query; 
 
     $result = mysqli_query($mysqli,$query);
     if (mysqli_fetch_assoc($result)) {
 
 
-        $_SESSION['user'] = $_POST['uname'];
+        $_SESSION['user'] = $_POST['username'];
         $_SESSION['loggedIn'] = true;
         header('location:header2.html');
     }
     else{
-        header('location:header.html');
+        header('location:../header.html');
     }
 
     }
